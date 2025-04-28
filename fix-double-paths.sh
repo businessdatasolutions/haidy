231218d49def68236709070d5307a183bc32b587
witusj <wfth69@gmail.com>
2025-04-28 15:10:23 +0200

    Simplified page

--- fix-double-paths.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Fix double haidy paths in all HTML files
-find . -name "*.html" -type f -exec sed -i '' -e 's|/haidy/haidy/|/haidy/|g' {} \;
-
-echo "Fixed double haidy paths in HTML files."
